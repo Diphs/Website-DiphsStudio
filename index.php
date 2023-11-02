4a3b5c4a22cd20389124a31c4b4846d44fe31440
Morgan <100171465+Diphs@users.noreply.github.com>
2023-11-02 18:14:03 +0700

    Up-desain

---
+++ index.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link href='https://unpkg.com/boxicons@2.0.9/css/boxicons.min.css' rel='stylesheet'>
+    <title>DiphsStudio</title>
+</head>
+<body>
+    <header>
+        <nav class="navbar">
+            <div class="container nav-wrapper">
+                <div class="menu-wrapper">
+                    <ul class="menu">
+                        <li class="menu-item"><a href="#" class="menu-link">Cara penggunaan</a></li>
+                        <li class="menu-item"><a href="#" class="menu-link">Tentang</a></li>
+                        <li class="menu-item"><a href="#" class="menu-link">Kontak</a></li>
+                    </ul>
+                </div>
+                <div class="menu-toggle bx bxs-grid-alt">
+            </div>
+            <a href="#" class="logo">Diphs<span>Studio</span></a>
+        </nav>
+    </header>
+
+    <div class="home">
+        <div class="up-img">
+            <div class="drag-area" ondrop="upload_file(event)" ondragover="return false">
+                <div class="icon"><i class="fas fa-cloud-upload-alt"></i></div>
+                    <div class='iconcam bx bxs-camera'></div>
+                    <button>Unggah Gambar</button>
+                    <header>Atau seret gambar ke sini</header>
+                <input type="file" name="file" id="file" hidden>
+            </div>
+
+            <div class="no-img">
+                <div class="label-noimg">
+                    <p class="label-asknoimg">Tidak ada gambar?</p>
+                    <p class="label-tryimg">Coba contoh berikut:</p>
+                </div>
+                <div class="sample-img">
+                    <div class="sample-img1">
+                        <img src="assets/gambar/sample-mobil.jpg" alt="">
+                    </div>
+                    <div class="sample-img2">
+                        <img src="assets/gambar/sample-orang.jpg" alt="">
+                    </div>
+                    <div class="sample-img3">
+                        <img src="assets/gambar/sample-hewan.jpg" alt="">
+                    </div>
+                    <div class="sample-img4">
+                        <img src="assets/gambar/sample-sepatu.jpg" alt="">
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="label-banner">
+            <div class="label-judul">
+                <p><span>Penghapus</span> latar belakang yang cepat dan sederhana untuk semua orang</p>
+            </div>
+
+            <div class="main-banner">
+                <img src="assets/gambar/main_banner 1.png" alt="">
+            </div>
+        </div>
+    </div>
+
+    <script src="assets/js/navbar.js"></script>
+    <script src="assets/js/home.js"></script>
+</body>
+</html>
---
+++ output_img.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link href='https://unpkg.com/boxicons@2.0.9/css/boxicons.min.css' rel='stylesheet'>
+    <title>DiphsStudio</title>
+</head>
+<body>
+    <header>
+        <nav class="navbar">
+            <div class="container nav-wrapper">
+                <div class="menu-wrapper">
+                    <ul class="menu">
+                        <li class="menu-item"><a href="#" class="menu-link">Cara penggunaan</a></li>
+                        <li class="menu-item"><a href="#" class="menu-link">Tentang</a></li>
+                        <li class="menu-item"><a href="#" class="menu-link">Kontak</a></li>
+                    </ul>
+                </div>
+                <div class="menu-toggle bx bxs-grid-alt">
+            </div>
+            <a href="#" class="logo">Diphs<span>Studio</span></a>
+        </nav>
+    </header>
+
+    <div class="form-outimg">
+        <div class="label-outimg">
+            <div class="label-before">
+                <a href="#">Sebelum</a>
+            </div>
+            <div class="label-after active">
+                <a href="#">Sesudah</a>
+            </div>
+        </div>
+        <div class="out-img">
+            <img src="assets/gambar/sample-hewan.jpg" alt="">
+        </div>
+        <div class="button-save">
+            <button>Simpan</button>
+        </div>
+    </div>
+
+    <script src="assets/js/navbar.js"></script>
+</body>
+</html>
