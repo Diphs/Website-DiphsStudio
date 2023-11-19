74297a1053fdb99013f8b336b9e14338c1cc7531
Morgan <100171465+Diphs@users.noreply.github.com>
2023-11-19 14:03:28 +0700

    Update cara penggunaan

---
+++ guide.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link href='https://unpkg.com/boxicons@2.0.9/css/boxicons.min.css' rel='stylesheet'>
+    <title>DiphsStudio</title>
+</head>
+<body>
+    <header>
+        <nav class="navbar">
+            <div class="container nav-wrapper">
+                <div class="menu-wrapper">
+                    <ul class="menu">
+                        <li class="menu-item"><a href="#" class="menu-link">Cara penggunaan</a></li>
+                        <li class="menu-item"><a href="#" class="menu-link">Tentang</a></li>
+                        <li class="menu-item"><a href="#" class="menu-link">Kontak</a></li>
+                    </ul>
+                </div>
+                <div class="menu-toggle bx bxs-grid-alt">
+            </div>
+            <a href="index.php" class="logo">Diphs<span>Studio</span></a>
+        </nav>
+    </header>
+
+    <div class="label-guide">
+        <div class="title-guide">
+            <h2>Bagaimana cara mengunakan Diphs<span>Studio</span></h2>
+        </div>
+        <div class="contents-guide">
+            <div class="guide">
+                <div class="fill-guide">
+                    <div class="step">1</div>
+                </div>
+                <div class="text-guide">
+                    <h3>Unggah</h3>
+                    <p>Langkah pertama, unggah gambar atau foto yang ingin Anda edit. Anda akan menemukan tombol Unggah Gambar" di halaman utama situs.</p>
+                </div>
+            </div>
+            <div class="guide">
+                <div class="fill-guide">
+                    <div class="step">2</div>
+                </div>
+                <div class="text-guide">
+                    <h3>Hapus</h3>
+                    <p>Setelah gambar diunggah,  maka akan memulai proses otomatis untuk menghapus latar belakang dari gambar tersebut.</p>
+                </div>
+            </div>
+            <div class="guide">
+                <div class="fill-guide">
+                    <div class="step">3</div>
+                </div>
+                <div class="text-guide">
+                    <h3>Download</h3>
+                    <p>Gambar yang sudah diedit akan muncul di layar. Anda akan memiliki opsi untuk mengunduh gambar yang sudah diedit. Klik tombol "Download", dan simpan gambar tanpa latar belakang ke perangkat Anda.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <script src="assets/js/navbar.js"></script>
+</body>
+</html>
